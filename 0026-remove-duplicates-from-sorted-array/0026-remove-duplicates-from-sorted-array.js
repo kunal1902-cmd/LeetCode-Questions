6a1a6338c9dbdda938a306e49bf00716fcda3d71
kunal1902-cmd <kp1544571@gmail.com>
2026-08-18 17:49:18 +0530

    Time: 68 ms (5.08%), Space: 56.6 MB (87.53%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+    for(i=0;i<nums.length;i++){
+        for(j=i+1;j<nums.length;j++){
+            if(nums[i]===nums[j]){
+                nums.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return nums.length;
+};
